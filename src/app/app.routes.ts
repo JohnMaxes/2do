72704b353239dc31ea704c0e93d7b95c099346a6
JohnMaxes <baodang9855.tdn@gmail.com>
2025-02-23 11:47:55 +0700

    added signup UI based on login UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { title: '2DO - Home', path: '', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)},
+    { title: '2DO - Home', path: '', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)}, // if you do component: HomeComponent, it will load the component immediately
     { title: '2DO - Todos', path: 'todos', loadComponent: () => import('./components/todos/todos.component').then(m => m.TodosComponent)},
-    { title: '2DO - Login', path: 'login', loadComponent: () => import('./components/auth/auth.component').then(m => m.AuthComponent)}
+    { title: '2DO - Login', path: 'login', loadComponent: () => import('./components/auth/auth.component').then(m => m.AuthComponent)},
+    { title: '2DO - Signup', path: 'signup', loadComponent: () => import('./components/signup/signup.component').then(m => m.SignupComponent)}
 ];
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -6,39 +6,46 @@ import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { HttpClient } from '@angular/common/http';
 
-
 @Component({
-  selector: 'app-auth',
+  selector: 'app-signup',
   imports: [NzInputModule, NzButtonModule, FormsModule, NzIconModule, ReactiveFormsModule],
-  templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
 })
-export class AuthComponent {
+export class SignupComponent {
   http = inject(HttpClient);
   username: string = 'john@mail.com';
+  email: string = 'johndoe@mail.com';
   password: string = 'changeme';
+  repeatPassword: string = 'changeme';
 
   loading = false;
 
+  passwordVisible: boolean = false;
+  repeatPasswordVisible: boolean = false;
   usernameRegex = /^[a-zA-Z0-9._%+-]{3,20}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  passwordRegex = /^(?=.{8,20}$)[a-zA-Z0-9!#$%^&*]+$/;  passwordVisible: boolean = false;
+  passwordRegex = /^(?=.{8,20}$)[a-zA-Z0-9!#$%^&*]+$/;  
+  emailRegex = /^[a-zA-Z0-9._%+-]{3,20}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   hasError: string = '';
   handleEnterKey(event: KeyboardEvent) {
-    if(event.key == 'Enter') this.submitLogin();
+    if(event.key == 'Enter') this.submitSignup();
   }
-  submitLogin() {
+  submitSignup() { // has same logic as login since there's no reliable way to test the signup
     this.hasError = '';
-    if (this.usernameRegex.test(this.username) == false) {
+    if (!this.usernameRegex.test(this.username)) {
       this.hasError = "User's fault!";
     } 
     if (!this.passwordRegex.test(this.password)) {
       this.hasError += " Password's fault!";
     }
-    if (this.hasError == '') {
+    if (this.password != this.repeatPassword) {
+      this.hasError += " Passwords don't match!";
+    }
+    if (this.hasError == '') { // if there's no error
       this.loading = true;
       const url = 'https://api.escuelajs.co/api/v1/auth/login';
-      this.http.post(url, { email: this.username, password: this.password }, { headers: { 'Content-Type': 'application/json' } }).subscribe(
+      this.http.post(url, { email: this.username, password: this.password }, { headers: { 'Content-Type': 'application/json' } }).subscribe( // subscribe == then but in Angular language
         (response: any) => {
           this.loading = false;
           if (response.access_token) {
